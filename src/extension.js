d58d6fd581593dadc93880a9b227af0efc6bd82f
Firas al-Khalil <firasalkhalil@gmail.com>
2023-07-28 14:29:30 +0200

    extension: tidy up extension activation

--- src/extension.js
+++ src/extension.js
@@ -33,21 +33,13 @@ function convert() {
 }
 
 export function activate(context) {
-
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "vscode-rpgfree" is now active!');
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
+  console.log('vscode-rpgfree activated');
   // The commandId parameter must match the command field in package.json
   context.subscriptions.push(
     vscode.commands.registerCommand('vscode-rpgfree.rpgleFree', function () {
       convert();
     })
   );
-
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() { }
